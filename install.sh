192ced0f973861673f7cde5eb0dd0a6de6a18716
JooYoung <sana2dang@naver.com>
2018-11-20 01:15:08 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,12 +3,15 @@ cp -f -r ./ /opt/retropie/configs/all/
 
 sudo chmod 755 /opt/retropie/configs/all/PauseMode/PauseMode
 sudo chmod 755 /opt/retropie/configs/all/PauseMode/pngview
+sudo chmod 755 /opt/retropie/configs/all/PauseMode/pngbg
 
 sudo sed -i '/PauseMode/d' /opt/retropie/configs/all/runcommand-onstart.sh
 echo '/opt/retropie/configs/all/PauseMode/PauseMode /dev/input/js0 10 11 0 &' >> /opt/retropie/configs/all/runcommand-onstart.sh
 
 sudo sed -i '/pngview/d' /opt/retropie/configs/all/runcommand-onend.sh
+sudo sed -i '/pngbg/d' /opt/retropie/configs/all/runcommand-onend.sh
 sudo sed -i '/PauseMode/d' /opt/retropie/configs/all/runcommand-onend.sh
 
 echo 'sudo killall pngview' >> /opt/retropie/configs/all/runcommand-onend.sh
+echo 'sudo killall pngbg' >> /opt/retropie/configs/all/runcommand-onend.sh
 echo 'sudo killall PauseMode' >> /opt/retropie/configs/all/runcommand-onend.sh
